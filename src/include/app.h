940f10983ae8e692a1a4fb6c8530ff9f86e1da34
Palash Bauri <palashbauri1@gmail.com>
2026-06-04 19:15:42 +0530

    project reset
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- src/include/app.h
+++
@@ -1,8 +0,0 @@
-#ifndef APP_H
-#define APP_H
-
-// Run the App. Entrypoint for zig and c main.
-// Todo: Take arguments
-int RunApp();
-
-#endif
--- src/main.zig
+++
@@ -1,7 +0,0 @@
-const app = @cImport({
-    @cInclude("app.h");
-});
-
-pub fn main() !void {
-    _ = app.RunApp();
-}
