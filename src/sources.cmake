221e4cac766e0bcdac7d0bd9e37773de8711d20f
Palash Bauri <palashbauri1@gmail.com>
2026-07-03 18:04:18 +0530

    convert tab to spaces in cmake stuff (for now)
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- src/sources.cmake
+++ src/sources.cmake
@@ -17,28 +17,28 @@
 #
 
 set(BAURIPIXEL_SRC_FILES
-	"${CMAKE_CURRENT_LIST_DIR}/app.c"
-	"${CMAKE_CURRENT_LIST_DIR}/components/panel.c"
-	"${CMAKE_CURRENT_LIST_DIR}/options.c"
-	"${CMAKE_CURRENT_LIST_DIR}/theme.c"
-	"${CMAKE_CURRENT_LIST_DIR}/widgets/colorpanel.c"
-	"${CMAKE_CURRENT_LIST_DIR}/widgets/drawarea.c"
-	
-	# External
-	"${CMAKE_CURRENT_LIST_DIR}/external/raylib/raygui_impl.c"
-	"${CMAKE_CURRENT_LIST_DIR}/external/stb/stb_ds_impl.c"
+    "${CMAKE_CURRENT_LIST_DIR}/app.c"
+    "${CMAKE_CURRENT_LIST_DIR}/components/panel.c"
+    "${CMAKE_CURRENT_LIST_DIR}/options.c"
+    "${CMAKE_CURRENT_LIST_DIR}/theme.c"
+    "${CMAKE_CURRENT_LIST_DIR}/widgets/colorpanel.c"
+    "${CMAKE_CURRENT_LIST_DIR}/widgets/drawarea.c"
+    
+    # External
+    "${CMAKE_CURRENT_LIST_DIR}/external/raylib/raygui_impl.c"
+    "${CMAKE_CURRENT_LIST_DIR}/external/stb/stb_ds_impl.c"
 )
 
 set(BAURIPIXEL_HEADER_FILES 
-	"${CMAKE_CURRENT_LIST_DIR}/alloc.h"
-	"${CMAKE_CURRENT_LIST_DIR}/btypes.h"
-	"${CMAKE_CURRENT_LIST_DIR}/colors.h"
-	"${CMAKE_CURRENT_LIST_DIR}/components.h"
-	"${CMAKE_CURRENT_LIST_DIR}/defaults.h"
-	"${CMAKE_CURRENT_LIST_DIR}/options.h"
-	"${CMAKE_CURRENT_LIST_DIR}/theme.h"
-	"${CMAKE_CURRENT_LIST_DIR}/widget.h"
-	"${CMAKE_CURRENT_LIST_DIR}/external/raylib/raygui.h"
+    "${CMAKE_CURRENT_LIST_DIR}/alloc.h"
+    "${CMAKE_CURRENT_LIST_DIR}/btypes.h"
+    "${CMAKE_CURRENT_LIST_DIR}/colors.h"
+    "${CMAKE_CURRENT_LIST_DIR}/components.h"
+    "${CMAKE_CURRENT_LIST_DIR}/defaults.h"
+    "${CMAKE_CURRENT_LIST_DIR}/options.h"
+    "${CMAKE_CURRENT_LIST_DIR}/theme.h"
+    "${CMAKE_CURRENT_LIST_DIR}/widget.h"
+    "${CMAKE_CURRENT_LIST_DIR}/external/raylib/raygui.h"
 )
 
 set(BAURIPIXEL_MAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/main.c")
